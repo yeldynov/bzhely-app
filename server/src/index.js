91cb0f5f717e9c8035872030208b5fff6af67620
yeldynov <yeldynov@gmail.com>
2023-06-03 16:00:38 +0300

    added auth routes, connected to mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,24 @@
+require('./models/User');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Mongo.');
+});
+mongoose.connection.on('error', () => {
+  console.log('Error connecting to Mongo.');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
